de63ae264a7b23422821048f206b10c5fbdcefbd
Olivier D <olivier.dauloudet@hotmail.com>
2022-03-17 20:49:19 +0100

    fix project name

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     description='put your description',
     long_description=readme,
     author='Dauloudet Olivier',
-    url='https://github.com/Smeaol22/docker-flask',
+    url='https://github.com/Smeaol22/docker_flask',
     license=my_license,
     package_dir={'': 'src'},
     packages=find_packages(exclude=('tests', 'docs', 'example', 'conda'))
